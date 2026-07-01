7189f60b8ae112c07fe6f9fadd163ef726efd2b3
Yadhira <yadhirasilva6@gmail.com>
2026-07-01 11:48:36 -0300

    feat: atuliazação na função de formatar data

--- utils/formatacao.py
+++ utils/formatacao.py
@@ -1,16 +1,26 @@
 import os
 
+
 def linha_separadora():
     print("-" * 60)
+
 
 def linha_dupla():
     print("=" * 60)
 
+
 def centralizar(texto):
     print(texto.center(60))
 
-def formatar_data_hora(data, hora):
-    return f"{data} às {hora}"
 
 def limpar_tela():
     os.system("clear")
+
+
+def formatar_data(criado_em):
+    try:
+        data, hora = criado_em.split(" ")
+        a, m, d = data.split("-")
+        return f"{d}/{m}/{a} às {hora[:5]}"
+    except Exception:
+        return criado_em
